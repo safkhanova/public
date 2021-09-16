4f154adc14af47ccb2be48c3ad46e389d51e2355
safkhanova <safxanova@gmail.com>
2021-09-16 23:27:06 +0200

    Chapter 6: Profile page and avatars - finished

--- app/models.py
+++ app/models.py
@@ -2,13 +2,15 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from hashlib import md5
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     '''
     backref argument defines the name of a field that will be added to the 
     objects of the "many" class that points back at the "one" object
@@ -19,6 +21,9 @@ class User(UserMixin, db.Model):
         self.password_hash=generate_password_hash(password)
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
